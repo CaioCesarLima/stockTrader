c5d34aa146f20d68186048657f3055af02aa75c2
CaioCesar <cesarlimacaio@gmail.com>
2020-03-18 01:33:28 -0300

    adionado modulo de portfolio na vuex

--- src/store/store.js
+++ src/store/store.js
@@ -2,11 +2,13 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 
 import stocks from './modulos/stocks'
+import portfolio from './modulos/portfolio'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
     modules:{
-        stocks
+        stocks,
+        portfolio
     }
 })
